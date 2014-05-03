e0b3a713b9153f89aeea213c17f8f70dedf11ab5
Allan Lavell <alavell@gmail.com>
2014-05-02 22:33:13 -0400

    Update glitch casting to reflect changes for Amazon S3.

--- config/castGlitch.js
+++ config/castGlitch.js
@@ -6,64 +6,84 @@ var mkpath = require('mkpath');
 var rootdir = require('../rootdir');
 
 module.exports = function(req, res) {
-  User.findOne({ 'email' : req.param('target_wizard') } , function(err, targetWizard) {
+  User.findOne({ '_id' : req.param('target_wizard') } , function(err, targetWizard) {
     if (!err) {
-      if (targetWizard !== null) {
-        if (isValidGlitch(req)) {
-          var glitch = new Glitch();
-          glitch.sourceWizard = req.user.id;
-          glitch.targetWizard = targetWizard.id;
-
-          var name = getGlitchName(req); 
-          var relPath = util.format('data/%s/%s', glitch.sourceWizard, glitch.targetWizard);
-          var dirPath = util.format('%s/%s', rootdir(), relPath);
-          var path = util.format('%s/%s', dirPath, name);
-          glitch.resourcePath = util.format('%s/%s', relPath, name);
-          console.log(name);
-          console.log(relPath);
-          console.log(dirPath);
-          console.log(path);
-          console.log(glitch.resourcePath);
-
-          mkpath(dirPath, function(err) {
-            if (err) throw err;
-
-            fs.readFile(req.files.image.path, function (err, data) {
-              fs.writeFile(path, data, function (err) {
-                if (err) throw err;
-              });
-            });
-          });
-          
-          glitch.save(function(err) {
-              if (err) throw err;
-          });
-        }
-      } 
+      if (!isValidGlitchType(req)) {
+        res.json({ 'status' : 
+          { 'name' : 'InvalidGlitchTypeError',
+            'message' : 'Invalid glitch image type. Must be jpeg or gif.'
+          }
+        });
+      }
+      else if (!isValidTarget(req, targetWizard)) {
+        res.json({ 'status' : 
+          { 'name' : 'InvalidTargetWizardError',
+            'message' : 'Invalid target wizard. Must be a wizard other than oneself.'
+          }
+        });
+      }
       else {
-        // let them know the target wizard doesn't exist 
-        console.log('invalid wizard: ' + req.param('target_wizard'));
+        var glitch = new Glitch();
+        glitch.sourceWizard = req.user.id;
+        glitch.targetWizard = targetWizard.id;
+
+        var name = getGlitchName(req); 
+        var relPath = util.format('https://s3.amazonaws.com/glitchwizard/%s/%s', glitch.sourceWizard, glitch.targetWizard);
+        var dirPath = util.format('%s/%s', rootdir(), relPath);
+        var path = util.format('%s/%s', dirPath, name);
+        glitch.resourcePath = util.format('%s/%s', relPath, name);
+
+        glitch.save(function(err) {
+            if (err) {
+              res.json(
+                { 'status' : {
+                  'name' : 'DatabaseError',
+                  'message' : err
+                  }
+                }
+              );
+              throw err;
+            }
+            else {
+              res.json({ 'status' : {
+                'name' : 'Success',
+                'message' : 'Glitch successfully entered into database.'
+               },
+                'filename' : name 
+              }
+              );
+            }
+        });
       }
     }
     else {
       console.log(err); 
+      res.json(
+      { 'status' : 
+        { 'name' : 'InvalidWizardError',
+          'message' : 'Invalid wizard.'
+        }
+      });
     }
   });
-  res.redirect('/profile');
 }
 
 var getGlitchName = function(req) {
   var date = new Date();
   var name;
-  if (req.files.image.type === 'image/jpeg') {
+  if (req.param('image_type') === 'image/jpeg') {
     name = util.format('%s.jpg', date.getTime());
   }
-  else {
+  else if (req.param('image_type') === 'image/gif') {
     name = util.format('%s.gif', date.getTime());
   }
   return name;
 }
 
-var isValidGlitch = function(req) {
-  return req.files.image && req.files.image.type === 'image/jpeg' || req.files.image.type === 'image/gif';
+var isValidGlitchType = function(req) {
+  return req.param('image_type') === 'image/jpeg' || req.param('image_type') === 'image/gif';
+}
+
+var isValidTarget = function(req, target_wizard) {
+  return req.user.id != target_wizard.id;
 }
