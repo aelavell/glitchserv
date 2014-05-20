e32950237bccab0a7e1c6518004340865067e338
Allan Lavell <alavell@gmail.com>
2014-05-20 12:27:27 -0400

    Debug the typeof target wizards

--- config/castGlitch.js
+++ config/castGlitch.js
@@ -16,6 +16,7 @@ extractTargetWizardIDs = function(req) {
   else {
     // TODO: make sure this actually returns an array in the proper format
     console.log(req.param('target_wizards'));
+    console.log(typeof(req.param('target_wizards')));
     return req.param('target_wizards');
   }
 }
